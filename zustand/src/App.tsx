637596b3d5189deb9131dcb507c23ab0ba317ae2
C_11nJxx <c_11njxxsmailbox@sina.com>
2025-03-28 10:29:02 +0800

    #

--- zustand/src/App.tsx
+++ zustand/src/App.tsx
@@ -1,11 +1,39 @@
 import { useUserStore } from "./store";
-export default function App() {
-  const useStore = useUserStore();
+
+function Form() {
+  const { username, email, setUserName, setEmail } = useUserStore();
 
   return (
-    <div className="flex flex-col items-center">
-      <div className="text-5xl text-blue-400">{useStore.username}</div>
-      <div className="text-4xl text-blue-500">{useStore.email}</div>
+    <div className="flex flex-col items-center gap-y-1">
+      <input
+        type="text"
+        value={username}
+        placeholder="username"
+        className="bg-gray-300 p-3 rounded-2xl"
+        onChange={(e) => {
+          setUserName(e.target.value);
+        }}
+      />
+      <input
+        type="text"
+        value={email}
+        placeholder="email"
+        className="bg-gray-300 p-3 rounded-2xl w-100 text-center"
+        onChange={(e) => {
+          setEmail(e.target.value);
+        }}
+      />
+    </div>
+  );
+}
+
+export default function App() {
+  const { username, email, setUserName, setEmail } = useUserStore();
+  return (
+    <div className="flex flex-col items-center gap-y-5">
+      <Form />
+      <div className="text-5xl text-gray-600 font-bold">{username}</div>
+      <div className="text-4xl text-gray-600 font-bold">{email}</div>
     </div>
   );
 }
